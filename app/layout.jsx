a3abc8b989d756d358e4a7380c05e993f483d3af
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-10-14 20:26:42 -0400

    Home Page - Incomplete

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,25 @@
-import '@styles/globals.css';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover and Share AI Prompts",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">{children}</main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
